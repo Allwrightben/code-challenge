a63b445af8dfb26e0f8aa793898caee2eff14d9e
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-11-25 19:54:23 +0000

    Project start - simple web page with input and output for code

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var $codeTextarea = $('#code');
+    var $outputIframe = $('#output');
+
+    $codeTextarea.on('input', function() {
+        var htmlCode = $(this).val();
+        updateIframeContent(htmlCode);
+    });
+
+    function updateIframeContent(htmlCode) {
+        $outputIframe.attr('src', 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlCode));
+    }
+});
