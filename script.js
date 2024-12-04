8d028e71be1a5ef609c1d19f5fd8f8632eabffa2
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-12-04 22:03:04 +0000

    Add themes and check code with messages

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-$(document).ready(function() {
-    var $codeTextarea = $('#code');
-    var $outputIframe = $('#output');
-
-    $codeTextarea.on('input', function() {
-        var htmlCode = $(this).val();
-        updateIframeContent(htmlCode);
-    });
-
-    function updateIframeContent(htmlCode) {
-        $outputIframe.attr('src', 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlCode));
-    }
-});
